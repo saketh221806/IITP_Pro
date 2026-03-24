45f2c0f62a2039179096c10be39b2b5e3e4fb18e
Sagitra <sagitrapradeep2006@gmail.com>
2026-03-25 00:47:51 +0530

    feat: initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div >Home</div>
+    <div>
+      <div className={`bg-[url(/backdrop.jpeg)] bg-fixed bg-cover h-screen bg-center flex items-center justify-center`}>
+        {/* hero section */ }
+        <h1 className='font-bold text-white text-5xl text-center p-20'>IIT Patna</h1>
+    </div>
+
+    {/*Sponsor*/}
+    <div className='flex justify-center'>
+      <h1 className='font-bold font-mono text-4xl'>SPONSOR</h1>
+
+    </div>
+    </div>
   )
 }
 
