8ae68297c1a7fe8121d72dc9ca4155b6f1448278
Sagitra <sagitrapradeep2006@gmail.com>
2026-03-25 22:29:27 +0530

    Home page created

--- src/pages/BookOfAbstracts.jsx
+++ src/pages/BookOfAbstracts.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const BookOfAbstracts = () => {
   return (
-    <div>BookOfAbstracts</div>
+    <div>
+      <div className={`bg-[url(/backdrop.jpeg)] bg-fixed bg-cover h-screen bg-center flex items-center justify-center`}>
+        {/* hero section */}
+        <h1 className='font-bold text-white text-5xl text-center p-20'>Book Of Abstracts</h1>
+      </div>
+    </div>
   )
 }
 
--- src/pages/ImportantDates.jsx
+++ src/pages/ImportantDates.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const ImportantDates = () => {
   return (
-    <div>ImportantDates</div>
+    <div>
+      <div className={`bg-[url(/backdrop.jpeg)] bg-fixed bg-cover h-screen bg-center flex items-center justify-center`}>
+        {/* hero section */}
+        <h1 className='font-bold text-white text-5xl text-center p-20'>Important Dates</h1>
+      </div>
+      <div className='flex justify-center flex-col items-center py-30'>
+      <ul className="list-disc list-inside text-left">
+        <li>Registration Opens – April 1, 2026</li>
+        <li>Submission Deadline – May 15, 2026</li>
+        <li>Review Results – June 10, 2026</li>
+        <li>Final Submission – June 25, 2026</li>
+        <li>Conference Date – July 10, 2026</li>
+      </ul>
+    </div>
+    </div>
+    
   )
 }
 
